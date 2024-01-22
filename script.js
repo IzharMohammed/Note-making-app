23794b5f4f011ba28902617d28af090831f490e2
MohammedIzhar77 <izharmohammed21@gmail.com>
2024-01-22 19:59:33 +0530

    added some comments

--- script.js
+++ script.js
@@ -7,13 +7,14 @@ function NotecreationDom(input) {
     oneNote.classList.add('oneNote')
     info.classList.add('info')
     button.classList.add('btn2')
-
-  const btnId = 'btn2-' + Date.now();
+    //Adding a unique so that for different notes the id should be different because in dynamic allocation of data there should not be
+    // elements with same id 
+    const btnId = 'btn2-' + Date.now();
     button.setAttribute('id', btnId);
 
     heading.textContent = "Note"
     button.textContent = "view detail"
- const fullinfo = input.value
+    const fullinfo = input.value
     info.textContent = input.value.substring(0, 35) + '...';
 
     oneNote.appendChild(heading)
@@ -21,23 +22,17 @@ function NotecreationDom(input) {
     oneNote.appendChild(button)
 
     const container = document.getElementById('container')
-
     container.append(oneNote)
-
-
-
-
-
-
+    //view details button
     const detailsbtn = document.getElementById(btnId)
-detailsbtn.addEventListener("click", (e)=>{
-    e.preventDefault()
- showDetails('Note', fullinfo )
-}
-)
+    detailsbtn.addEventListener("click", (e) => {
+        e.preventDefault()
+        showDetails('Note', fullinfo)
+    }
+    )
 }
 // NotecreationDom()
-
+//save button
 const savebtn = document.getElementById('btn1')
 savebtn.addEventListener("click", () => {
     const input = document.getElementById('input')
@@ -48,14 +43,14 @@ savebtn.addEventListener("click", () => {
     }
     input.value = ''
 })
-
-function showDetails(heading,information){
-    const info = document.getElementById('content').innerText=information;
-    const title = document.getElementById('popup-title').innerText=heading;
-    const dispay = document.getElementById('popup').style.display='block'
+//view details button
+function showDetails(heading, information) {
+    const info = document.getElementById('content').innerText = information;
+    const title = document.getElementById('popup-title').innerText = heading;
+    const dispay = document.getElementById('popup').style.display = 'block'
     const closebtn = document.getElementById('closebtn')
-    closebtn.addEventListener('click',()=>{
-        document.getElementById('popup').style.display='none'
+    closebtn.addEventListener('click', () => {
+        document.getElementById('popup').style.display = 'none'
     })
-console.log(information)
+    console.log(information)
 }
